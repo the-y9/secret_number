223e52fff9fbe5b0280564560a3a2ccc97c8ec43
Yash Mishra <ymishra975@gmail.com>
2026-05-13 22:57:32 +0530

    start, reset, fireworks

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
 let low = 1;
-let high = 50;
-let gn = 0;
+let high = Math.floor(Math.random() * 100) + 1;
+let gn = 1;
 
 let phase = 1; // 1 = exponential search, 2 = binary search
 let gameOver = false;
-
-const guessText = document.getElementById("guess");
+let gameStarted = false;
+if (gameStarted === true) {
+  
+}
+const startBtn = document.getElementById("start");
 const attemptsText = document.getElementById("attempts");
+const gameControls = document.getElementById("gameControls");
+const guessText = document.getElementById("guess");
+
+function startGame() {
+  gameControls.style.display = "block";
+  startBtn.style.display = 'none';
+  gameStarted = true;
+  resetGame();
+
+}
+
+function resetGame() {
+  if (gameStarted) {
+    low = 1;
+    high = Math.floor(Math.random() * 100) + 1;
+    gn = 1;
+    phase = 1;
+    gameOver = false;
+    guessText.innerText = `My guess is: ${currentGuess()}`;
+    attemptsText.innerText = `Attempts: ${gn}`;
+  }
+}
+
 
 function updateUI(message, isGuess = true) {
     guessText.innerText = message;
@@ -58,7 +84,7 @@ function handleResponse(response) {
     else if (response === "correct" || response === "yes") {
 
       updateUI(`Yay! I guessed it in ${gn} attempt(s).`, false);
-
+      fireworks();
       gameOver = true;
     }
 
@@ -112,5 +138,25 @@ function handleResponse(response) {
   updateUI(`My guess is: ${currentGuess()}`, true);
 }
 
-// initial guess
-updateUI(`My guess is: ${currentGuess()}`, true);
+function fireworks() {
+
+  confetti({
+    particleCount: 200,
+    spread: 120,
+    origin: { y: 0.6 }
+  });
+
+  confetti({
+    particleCount: 150,
+    angle: 60,
+    spread: 80,
+    origin: { x: 0 }
+  });
+
+  confetti({
+    particleCount: 150,
+    angle: 120,
+    spread: 80,
+    origin: { x: 1 }
+  });
+}
